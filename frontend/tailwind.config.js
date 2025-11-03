4a27dd0d9c80f1c5e0abd53124d3584401883435
joseo21 <54163648+joseo21@users.noreply.github.com>
2025-11-03 11:18:53 -0300

    Delete frontend directory

--- frontend/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: 'class', // 👈 agrega esta línea
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
